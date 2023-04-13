d10d420e532e382658ad27e0c76c53305238de03
franco-rivarola-davinci <89221841+franco-rivarola-davinci@users.noreply.github.com>
2023-04-13 13:46:36 -0300

    Add files via upload

---
+++ reconocimientoFacial2/openCvConfig.py
@@ -0,0 +1,10 @@
+from opencv.fr import FR
+import cv2
+
+BACKEND_URL = "https://us.opencv.fr"
+DEVELOPER_KEY = "eAWupfPYWIxMDUzMTUtZTljZS00MTk4LWE3YzMtYzE1YWVkMDEyMjM4"
+
+sdk = FR(
+    BACKEND_URL,
+    DEVELOPER_KEY
+)
